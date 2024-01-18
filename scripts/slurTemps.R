3571aa76d680cd52f382cc0fdd5df7cdb52f9549
Frederik Rask Dalby <au277187@uni.au.dk>
2024-01-18 22:34:26 +0100

    temp update

--- scripts/slurTemps.R
+++ scripts/slurTemps.R
@@ -11,7 +11,11 @@ dat$month <- month(dat$date)
 dat$doy <- as.numeric(as.Date(dat$date) - as.Date("2023-01-01"))
 dat$fill <- dat$m3/dat$capacity
 
-dat.mod <- dat %>% filter(!grepl('D',Tank)) %>% group_by(month) %>% 
+dat.count <- data.frame(table(dat$month[!is.na(dat$temp) & !grepl('D', dat$Tank)]) )
+
+rem <- as.numeric(dat.count$Var1[which(dat.count$Freq <= 1)]) # June only one temp -> interpolate instead. 
+
+dat.mod <- dat %>% filter(!grepl('D',Tank), !is.na(temp)) %>% group_by(month) %>% 
   summarise(doy = mean(doy, na.rm = T), 
             temp = mean(temp, na.rm = T),
             temp_sd = sd(temp, na.rm = T),
@@ -21,7 +25,9 @@ dat.mod <- dat %>% filter(!grepl('D',Tank)) %>% group_by(month) %>%
 # mean doy of each month
 mean_month_doy <- c(15, 45, 76, 106, 137, 167, 198, 228, 259, 289, 320, 350)
 dat.mod$mean_month_doy <- mean_month_doy
-dat.mod$temp_mean_month_doy <- approx(x = dat.mod$doy, y = dat.mod$temp, xout = dat.mod$mean_month_doy)$y
+
+# interpolate but exclude the June measurement.
+dat.mod$temp_mean_month_doy <- approx(x = dat.mod$doy[-rem], y = dat.mod$temp[-rem], xout = dat.mod$mean_month_doy)$y
 
 int_dat <- rbind(dat.mod[c(11:12),],dat.mod[c(1:2),])  
 int_dat$doy_cor <- c(int_dat$doy[1:2] - 365, int_dat$doy[3:4]) - min(c(int_dat$doy[1:2] - 365, int_dat$doy[3:4]))
@@ -38,7 +44,7 @@ write.csv(dat.out, "../data/temp_dat.csv", row.names = F)
 
 plot(dat.out$mean_month_doy, dat.out$temp_mean_month_doy)
 
-
+mean(dat.out$temp_mean_month_doy)
 
 
 
